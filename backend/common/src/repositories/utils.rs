ad4939a40c98cac47b6a245c4c49f9e852048125
Serge Weinstock <serge.weinstock@gmail.com>
2024-10-06 09:26:31 +0100

    code cleanup

--- backend/common/src/repositories/utils.rs
+++ backend/common/src/repositories/utils.rs
@@ -1,18 +1,15 @@
 use diesel::{connection::SimpleConnection, PgConnection};
 use std::{error::Error, path::PathBuf};
 
-/// Run s sql script. If [path] starts with a '/',
-/// a relative path is used, return an error
+/// Run s sql script.
+/// if [path] is relative, it will be resolved relative to the project root.
 pub fn run_sql_script(path: String, conn: &mut PgConnection) -> Result<(), Box<dyn Error>> {
-    let mut d: PathBuf;
-    if !path.starts_with('/') {
-        d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        d = d.join(path);
-    } else {
-        d = PathBuf::from(path);
+    let mut path = PathBuf::from(path);
+    if path.is_relative() {
+        path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(path);
     }
-    println!("Running SQL script: {:?}", d);
-    let sql = std::fs::read_to_string(d)?;
+    println!("> Running SQL script: {:?}", path);
+    let sql = std::fs::read_to_string(path)?;
     let _ = conn.batch_execute(sql.as_str());
 
     Ok(())
