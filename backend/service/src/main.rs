02f428c58a5a142fe766768aa21daeb0de2243eb
Serge Weinstock <serge.weinstock@gmail.com>
2024-10-24 16:43:04 +0100

    First skeleton for the rest server

--- backend/service/src/main.rs
+++ backend/service/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
+use dotenvy::dotenv;
+use rocket_db_pools::{Connection, Database};
+use routes::DbConn;
+
+pub mod routes;
+
+#[rocket::get("/")]
+fn sample_main(_db: Connection<DbConn>) {
     println!("Hello, world!");
+}
+
+#[rocket::main]
+async fn main() {
+    dotenv().ok();
+    for (key, value) in std::env::vars() {
+        println!("=> '{}': '{}'", key, value);
+    }
+    let _ = rocket::build()
+        .mount("/", rocket::routes![sample_main,])
+        .attach(routes::CacheConn::init())
+        .attach(routes::DbConn::init())
+        .launch()
+        .await;
 }
---
+++ backend/service/src/routes/mod.rs
@@ -0,0 +1,7 @@
+#[derive(rocket_db_pools::Database)]
+#[database("postgres")]
+pub struct DbConn(rocket_db_pools::diesel::PgPool);
+
+#[derive(rocket_db_pools::Database)]
+#[database("redis")]
+pub struct CacheConn(rocket_db_pools::deadpool_redis::Pool);
